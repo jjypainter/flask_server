8d62d2d5afa7be8b0fb09f02f2363331fea0e03a
jjypainter <jypainter0612@gmail.com>
2019-12-20 11:45:23 +0900

    깃이그노 파일내용 깃이그노 사이트에서 추가. 헬로 파일 추가 핑하고 퐁 파일 생성

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request,render_template
 import random
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -18,3 +20,67 @@ def lunch():
     menus=['양자강','김밥까페','20층','순남시래기']
     lunch=random.choice(menus)
     return lunch
+
+#아이돌 백과사전
+@app.route('/idol')
+def idol():
+    idols={
+        'bts':{
+            '지민':25,
+            '랩몬스터':23
+        },
+        'rv':'레드벨벳',
+        '핑클':{
+            '이효리':'거꾸로해도 이효리',
+            '옥주현':'35'
+        },
+        'SES':['유진','바다','슈']
+    }
+    return idols
+
+@app.route('/post/<int:num>')
+def post(num):
+    posts=['0번 포스트','1번 포스트','2번 포스트']
+    return posts[num]
+
+#실습 cube뒤에 전달된 수의 세제곱수를 화면에 보여주세요.
+#1 ->1, 2->8, 3->27
+#str():숫자를 문자로 바꿔주는 함수
+@app.route('/cube/<int:num>')
+def cube(num):
+    cubed=num*num*num  
+    return str(cubed)
+
+#클라이언트에게 html파일을 주고 싶어요.
+@app.route('/html')
+def html():
+    return render_template('hello.html')
+
+
+@app.route('/ping')
+def ping():
+    return render_template('ping.html')
+
+@app.route('/pong')
+def pong():
+    age=request.args.get('age')
+    #age=request.args('age')
+    return render_template('pong.html',age_in_html=age)
+
+#로또번호를 가져와서 보여주는 서버
+@app.route('/lotto_result/<int:round>')
+def lotto_result(round):
+    url=f'https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo={round}'
+    result=requests.get(url).json()
+
+    winner=[]
+    for i in range(1,7):
+        winner.append(result.get(f'drwNo{i}'))
+
+    winner.append(result.get('bnusNo'))
+
+    return json.dumps(winner)
+
+
+#https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=800
+app.run(debug=True)
